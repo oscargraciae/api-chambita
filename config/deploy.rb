dc2809ed12de916f120e3e9fe81e03f4149bcc67
Oscar Gracia <oscar@chambita.mx>
2016-07-21 02:07:55 -0500

    Prueba de deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :branch, :master
 set :deploy_to, '/home/deploy/api-chambita'
 set :pty, true
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{bin log tmp/cache vendor/bundle public/system public/uploads}
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 #set :rvm_type, :user
