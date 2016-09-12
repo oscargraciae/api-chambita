9260dd4d1c0694f2281ecdab3a7ae1c9e5ab1cad
Oscar Gracia <oscar@chambita.mx>
2016-09-12 16:53:29 -0500

    deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,8 +61,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-
   before_create :generate_authentication_token!
 
   do_not_validate_attachment_file_type :avatar
