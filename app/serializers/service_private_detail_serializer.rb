f469f11416f24031d7b46e7088138914d6a63d2d
Oscar Gracia <oscar@chambita.mx>
2016-10-05 20:04:39 -0500

    deploy

--- app/serializers/service_private_detail_serializer.rb
+++ app/serializers/service_private_detail_serializer.rb
@@ -1,6 +1,6 @@
 class ServicePrivateDetailSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :description, :price, :cover, :rating_general, :service_ratings, :unit_max, :published, :created_at, :updated_at
+  attributes :id, :name, :description, :price, :cover, :rating_general, :service_ratings, :unit_max, :published, :created_at, :updated_at, :visits
 
   has_one :category
   has_one :sub_category
