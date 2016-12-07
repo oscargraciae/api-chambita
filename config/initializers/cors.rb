3d25a33541537dd104fa53bc414ced4eb74824f0
Oscar Gracia <oscar@chambita.mx>
2016-12-07 17:05:45 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,5 +14,8 @@ Rails.application.config.middleware.insert_before 0, 'Rack::Cors' do
   #            methods: [:get, :post, :put, :patch, :delete, :options, :head]
   # end
     origins '*'
+
     resource '*', headers: :any, methods: :any
+
+  end
 end
