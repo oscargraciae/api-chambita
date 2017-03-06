00159427a58acd0ae2b225f6ab67c0cd14a15f11
Oscar Gracia <oscar@chambita.mx>
2017-03-06 13:14:43 -0600

    Deploy - Ocultar correo y telefono en los mensajes de cliente y proveedores

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2,10 +2,6 @@ class Api::V1::ReportsController < BaseController
     before_filter :auth, only: [:sales, :total_sales]
 
     def sales
-        #pending = RequestService.where(supplier_id: @user.id, request_status_id: REQUEST_STATUS_PENDING).sum(:subtotal)
-        # pending = RequestService.where('supplier_id = ? AND (request_status_id = ? OR request_status_id = ? )', @user.id, 1, 2).sum(:subtotal)
-        # sold = RequestService.where('supplier_id = ? AND request_status_id = ? ', @user.id, 3).sum(:subtotal)
-        
         pending = Order.joins("INNER JOIN request_services r on orders.request_service_id = r.id").where('r.supplier_id = ? AND order_status_id = ?', @user.id, 3).sum(:subtotal)
         sold = Order.joins("INNER JOIN request_services r on orders.request_service_id = r.id").where('r.supplier_id = ? AND order_status_id = ?', @user.id, 1).sum(:subtotal)
         total = sold + pending
