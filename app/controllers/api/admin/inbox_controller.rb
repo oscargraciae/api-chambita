b90bfe2616c7c007e88d43d9daa105f557a74790
Oscar Gracia <oscar@chambita.mx>
2017-03-06 21:47:02 -0600

    deploy

--- app/controllers/api/admin/inbox_controller.rb
+++ app/controllers/api/admin/inbox_controller.rb
@@ -1,6 +1,6 @@
 class Api::Admin::InboxController < ApplicationController
   def index
-    messages = Inbox.all
+    messages = Inbox.all.order(id: :desc)
     render json: messages, status: :ok
   end
 end
