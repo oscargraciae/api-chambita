4eb332921f414cfa06082285cfbb0bbd0b55a5a8
Oscar Gracia <oscar@chambita.mx>
2017-03-12 21:11:54 -0600

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,8 @@ set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock" # accept array for
 # set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 
 set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 
